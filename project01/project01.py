03658d76b042c1abb63d7e50141538ed36efd6de
abigailackor1 <ackor.a@northeastern.edu>
2026-09-15 19:27:57 +0000

    first draft of project01.py

--- project01/project01.py
+++ project01/project01.py
@@ -3,13 +3,52 @@ from pprint import pprint
 
 
 # Modify this function signature and fill in the details
-def parse_line()
-    pass
+#Begin by spliting the data by tabs, then split the 8th column by semicolons, 
+# and finally split each of those by equals signs to create a dictionary of key-value pairs.
+def parse_line(str):
+    columns = str.split("\t")
+    info = columns[7]
+    info_pairs = info.split(";")
+    
+    af_exac_check = None
+    clndn = None
+
+    for pair in info_pairs:
+        key_value = pair.split("=")
+        key = key_value[0]
+
+        if len(key_value) < 2:
+            continue
+
+        value = key_value[1]
+
+        if key == "AF_EXAC":
+                af_exac_check = float(value)
+        elif key == "CLNDN":
+                clndn = value
+        return af_exac_check, clndn
+
+    if af_exac_check is None:
+        return []
+    
+    if af_exac_check >= 0.0001:
+        return []
+
+    diseasescase = clndn.split("|") 
+    filtered_diseases = []
+    for disease in diseasescase:
+        if disease != "not_provided" and disease != "not_specified":
+            filtered_diseases.append(disease)
+    return filtered_diseases
+
+
+            
+        
 
 
 # Modify this function signature and fill in the details
-def read_file()
-    pass
+def read_file(filename):
+
 
 
 if __name__ == "__main__":
